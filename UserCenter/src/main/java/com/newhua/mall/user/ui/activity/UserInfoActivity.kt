b714230dfaafec82f5439b4374c27ea4bb35c4af
nkqhxj <nkqhxj@163.com>
2018-06-11 17:21:24 +0800

    用户头像拍照或从相册选择图片功能实现

--- UserCenter/src/main/java/com/newhua/mall/user/ui/activity/UserInfoActivity.kt
+++ UserCenter/src/main/java/com/newhua/mall/user/ui/activity/UserInfoActivity.kt
@@ -1,10 +1,19 @@
 package com.newhua.mall.user.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import com.bigkoo.alertview.AlertView
 import com.bigkoo.alertview.OnItemClickListener
+import com.jph.takephoto.app.TakePhoto
+import com.jph.takephoto.app.TakePhotoImpl
+import com.jph.takephoto.compress.CompressConfig
+import com.jph.takephoto.model.TResult
 import com.newhua.mall.base.ext.onClick
 import com.newhua.mall.base.ui.activity.BaseMvpActivity
+import com.newhua.mall.base.utils.DateUtils
 import com.newhua.mall.user.R
 import com.newhua.mall.user.injection.component.DaggerUserComponent
 import com.newhua.mall.user.injection.module.UserModule
@@ -12,18 +21,26 @@ import com.newhua.mall.user.presenter.UserInfoPresenter
 import com.newhua.mall.user.presenter.view.UserInfoView
 import kotlinx.android.synthetic.main.activity_user_info.*
 import org.jetbrains.anko.toast
+import java.io.File
 
 /**
  * 用户信息
  */
-class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
+class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, TakePhoto.TakeResultListener {
+
+    private lateinit var mTakePhoto: TakePhoto
+
+    private lateinit var mTempFile: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
         injectComponent()
 
+        mTakePhoto = TakePhotoImpl(this, this)
         initView()
+
+        mTakePhoto.onCreate(savedInstanceState)
     }
 
     private fun initView() {
@@ -37,9 +54,13 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
         AlertView("选择图片", "", "取消", null, arrayOf("拍照", "相册"), this,
                 AlertView.Style.ActionSheet, object: OnItemClickListener {
             override fun onItemClick(o: Any?, position: Int) {
+                mTakePhoto.onEnableCompress(CompressConfig.ofDefaultConfig(), false)
                 when(position) {
-                    0 -> toast("拍照")
-                    1 -> toast("相册")
+                    0 -> {
+                        createTempFile()
+                        mTakePhoto.onPickFromCapture(Uri.fromFile(mTempFile))
+                    }
+                    1 -> mTakePhoto.onPickFromGallery()
                 }
             }
         }).show()
@@ -58,5 +79,34 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
 
     override fun onUserInfoResult(result: String) {
 
+    }
+
+    override fun takeSuccess(result: TResult?) {
+        Log.d("TakePhoto", result?.image?.originalPath)
+        Log.d("TakePhoto", result?.image?.compressPath)
+    }
+
+    override fun takeCancel() {
+
+    }
+
+    override fun takeFail(result: TResult?, msg: String?) {
+        Log.e("tackePhoto", msg)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mTakePhoto.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun createTempFile() {
+        val tempFileName = "${DateUtils.curTime}.png"
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            this.mTempFile = File(Environment.getDataDirectory(), tempFileName)
+            return
+        }
+
+        this.mTempFile = File(filesDir, tempFileName)
     }
 }
